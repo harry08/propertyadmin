353dbb704d38433090d70de6e13f0bdf15888620
harry08 <ha.huebner@nexgo.de>
2018-11-29 07:16:49 +0100

    Creation of initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http'
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PropertiesComponent } from './properties/properties.component';
+import { PropertyListComponent } from './properties/property-list/property-list.component';
+import { PropertyDetailComponent } from './properties/property-detail/property-detail.component';
+import { PropertyItemComponent } from './properties/property-list/property-item/property-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PropertiesComponent,
+    PropertyListComponent,
+    PropertyDetailComponent,
+    PropertyItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/properties/properties.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-properties',
+  templateUrl: './properties.component.html',
+  styleUrls: ['./properties.component.css']
+})
+// Overall component holding the list on the left and the detail on the right.
+export class PropertiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

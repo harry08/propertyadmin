62318dfcdcc3e36ff99df77d087d10aa6318458a
harry08 <ha.huebner@nexgo.de>
2018-11-29 17:47:13 +0100

    Property list and detail

--- src/app/properties/property-list/property-list.component.ts
+++ src/app/properties/property-list/property-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Property } from '../propery.model';
 
 @Component({
   selector: 'app-property-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
+  properties: Property[] = [
+    new Property("username", "Name of the user", "k001234"),
+    new Property("outputpath", "Path to output folder", "./test/output"),
+    new Property("inputfile", "Path to input file", "./test/test.csv")
+  ];
 
   constructor() { }
 
---
+++ src/app/properties/propery.model.ts
@@ -0,0 +1,11 @@
+export class Property {
+    public name: string;
+    public description: string;
+    public value: string;
+
+    constructor(name: string, description: string, value: string) {
+        this.name = name;
+        this.description = description;
+        this.value = value;
+    }
+}
